d8582a9404b1df24479725c29631300f0a975073
Denis Lapuente <darthsuicune@gmail.com>
2017-03-09 12:49:04 +0100

    Start tests

--- discordbot_test.go
+++ discordbot_test.go
@@ -5,7 +5,11 @@ import (
 )
 
 func TestGuild_Save(t *testing.T) {
-
+	addRaid(RANCOR, EU)
+	savedData := guilds[EU].Save()
+	if savedData == nil {
+		t.Error("Wat")
+	}
 }
 
 func TestGuild_Restore(t *testing.T) {
